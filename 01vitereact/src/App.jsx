7fb258a1d1f472404999643e16ac37c6e19f80dd
M3etp <meet.wsf@gmail.com>
2023-10-01 20:25:06 +0530

    leaned how to clean code and modify

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Meet Parmar | React 1st page | Hello World </h1>
+    </>
+  )
+}
+
+export default App
