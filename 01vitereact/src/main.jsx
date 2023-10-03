d421f14d12b6ba3f818f5f1acf100c0870edf62b
M3etp <meet.wsf@gmail.com>
2023-10-03 20:23:01 +0530

    variable passing through the function of react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+
+    const username = "chai aur code"
+    return(
+        <div>
+           this is {username}
+        </div>
+    )
+}
+
+const ReactElement = React.createElement(
+    "a",
+    {href: "https://google.com"},
+    "visit teh google"
+) 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App />
+    <MyApp />
   
 )
