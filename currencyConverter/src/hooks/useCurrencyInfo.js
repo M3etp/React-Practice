0be4f83f6e9ac092273979e5185a7a1305115227
M3etp <meet.wsf@gmail.com>
2023-10-12 15:24:44 +0530

    conerter app

--- currencyConverter/src/hooks/useCurrencyInfo.js
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-function useCurrencyInfo(currency){
+const useCurrencyInfo=function (currency){
     const [data, setData] = useState({})
     useEffect(()=>{
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
