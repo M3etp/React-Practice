afcb884581a6bb4eb00061fed1538a9cbaccc054
M3etp <meet.wsf@gmail.com>
2023-10-29 12:25:34 +0530

    env variable

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,15 @@
+import config from './config/config'
+import './App.css'
+
+function App() {
+  
+  console.log(config.appwriteUrl)
+
+  return (
+    <>
+     <h1>Mega Project In React</h1>
+    </>
+  )
+}
+
+export default App
