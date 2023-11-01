749a4d7543001a96d7137292f5a2676182373777
M3etp <meet.wsf@gmail.com>
2023-11-01 23:37:01 +0530

    react hook form applied

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,11 +3,19 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./logo";
 import LogoutButton from "./Header/LogoutButton";
+import Button from './Button'
+import Input from "./Input";
+import Select from "./Select";
+import PostCard from "./PostCard";
 
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutButton
+    LogoutButton,
+    Button,
+    Input,
+    Select,
+    PostCard
 }
